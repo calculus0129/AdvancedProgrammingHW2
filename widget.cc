56f9cc75090ca0b4937c9bdeeedaed46c2575b3e
calculus0129 <ryanbhang129@naver.com>
2023-05-29 01:39:41 +0900

    feat: vertical_split class with The Rule of 3
    main05.cc covered!

--- widget.cc
+++ widget.cc
@@ -15,3 +15,18 @@ char stretchy::char_at(unsigned int x, unsigned int y, unsigned int width, unsig
     if(x<0 || y<0 || x>=width || y>=height) return ' '; // exceptional case. Not considered in the assignment02.pdf.
     return (y==0 || y==height-1)?'.':((y==1 || y==height-2)?':':'|');
 }
+
+char vertical_split::char_at(unsigned int x, unsigned int y, unsigned int width, unsigned int height) const {
+    unsigned int h = (height>>1);
+    return y<h?t->char_at(x, y, width, h):b->char_at(x, y-h, width, height-h);
+}
+
+vertical_split& vertical_split::operator=(const vertical_split &o) {
+    if(t!=nullptr) {
+        delete t;
+        delete b;
+    }
+    t = o.t->copy();
+    b = o.b->copy();
+    return *this;
+}
--- widget.h
+++ widget.h
@@ -6,6 +6,7 @@ class widget {
     public:
     virtual ~widget() {}
     virtual char char_at(unsigned int x, unsigned int y, unsigned int width, unsigned int height) const=0;
+    virtual widget* copy() const=0;
     //friend void print();
 };
 
@@ -15,6 +16,7 @@ class label : public widget {
     label(std::string s)
     : s{s} {}
     char char_at(unsigned int x, unsigned int y, unsigned int width, unsigned int height) const;
+    widget* copy() const {return new label(*this);}
 };
 
 class checkerboard : public widget {
@@ -25,10 +27,25 @@ class checkerboard : public widget {
 
     }
     char char_at(unsigned int x, unsigned int y, unsigned int width, unsigned int height) const;
+    widget* copy() const {return new checkerboard(*this);}
 };
 
 class stretchy : public widget {
     char char_at(unsigned int x, unsigned int y, unsigned int width, unsigned int height) const;
+    public: widget* copy() const { return new stretchy(*this); }
+};
+
+class vertical_split : public widget {
+    widget *t, *b;
+    char char_at(unsigned int x, unsigned int y, unsigned int width, unsigned int height) const;
+    public:
+    vertical_split(widget *a, widget *b): t(a), b{b} {}
+    ~vertical_split() { delete t; delete b; }
+    // Copy Constructor
+    vertical_split(const vertical_split& o) : t(o.t->copy()), b{o.b->copy()} {}
+    // Assignment Operator
+    vertical_split& operator=(const vertical_split &o);
+    widget* copy() const { return new vertical_split(*this); };
 };
 
 #endif
